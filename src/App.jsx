00c5d91327c65d451f7cb508b8d3e1313eed90ea
yosket <yosket87@gmail.com>
2024-09-26 02:59:21 +0300

    feat: initial commit with project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="text-4xl">Hello there</div>
+    </>
+  );
+}
+
+export default App;
